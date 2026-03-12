21798e69ca617417e3b291dcfd20322e0edee2f7
Marat <akhmetzyanovmm@gmail.com>
2026-03-12 15:17:46 +0300

    refactor: simplify shape imports via package exports

---
+++ vector_editor/shapes/__init__.py
@@ -0,0 +1,7 @@
+from vector_editor.shapes.base import Shape
+from vector_editor.shapes.circle import Circle
+from vector_editor.shapes.point import Point
+from vector_editor.shapes.segment import Segment
+from vector_editor.shapes.square import Square
+
+__all__ = ["Shape", "Point", "Segment", "Circle", "Square"]
