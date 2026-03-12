97001deef10f260b7d344984adc6bd991ea89f0b
Marat <akhmetzyanovmm@gmail.com>
2026-03-12 15:45:34 +0300

    refactor: extract shape factories into separate module

--- vector_editor/commands.py
+++ vector_editor/commands.py
@@ -1,38 +1,9 @@
 from typing import Callable
 
 from vector_editor.repository import ShapeRepository
-from vector_editor.shapes import Circle, Point, Segment, Shape, Square
+from vector_editor.shape_factories import SHAPE_FACTORIES
 
 CommandHandler = Callable[[list[str], ShapeRepository], None]
-ShapeFactory = Callable[[list[str]], Shape]
-
-
-def create_point(args: list[str]) -> Point:
-    x, y = map(float, args)
-    return Point(x, y)
-
-
-def create_segment(args: list[str]) -> Segment:
-    x1, y1, x2, y2 = map(float, args)
-    return Segment(x1, y1, x2, y2)
-
-
-def create_circle(args: list[str]) -> Circle:
-    x, y, r = map(float, args)
-    return Circle(x, y, r)
-
-
-def create_square(args: list[str]) -> Square:
-    x, y, side = map(float, args)
-    return Square(x, y, side)
-
-
-SHAPE_FACTORIES: dict[str, ShapeFactory] = {
-    "point": create_point,
-    "segment": create_segment,
-    "circle": create_circle,
-    "square": create_square,
-}
 
 
 def create_command(args: list[str], repo: ShapeRepository) -> None:
